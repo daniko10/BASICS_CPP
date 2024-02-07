c85d6e72bd1a9f57f868bbadec257b09fc3d1342
Daniel <dczapla79@gmail.com>
2024-02-07 15:02:52 +0100

    Second labs with CMAKE

---
+++ LAB_2/include/GSeries.h
@@ -0,0 +1,11 @@
+#ifndef GSERIES_H
+#define GSERIES_H
+
+#include <iostream>
+
+typedef int rozmiarCiagu;
+typedef double* ciag;
+typedef double (*operacja)(ciag, rozmiarCiagu);
+//(*(ptr[]))()
+
+#endif
